ebaf692c55053e41ebe6d2c70e11c74038f6814f
malumendonca998 <luizqmorais94@gmail.com>
2024-07-26 10:28:57 -0300

    Update desafio_Dio.py

--- desafio_Dio.py
+++ desafio_Dio.py
@@ -1,5 +1,6 @@
 menu = """
-
+[c] Criar usuário
+[cc] Criar conta corrente
 [d] Depositar
 [e] Extrato
 [s] Sacar
@@ -12,11 +13,48 @@ limite = 500
 extrato = ""
 num_saques = 0
 LIM_SAQUE = 3
+agencia = "0001"
+num_conta = 1
+
+usuarios = []
+contas_correntes = []
+
+def encontrar_usuario(cpf,usuarios):
+    for i in usuarios:
+        if i["cpf"] == cpf:
+            return i
+    return None
 
 while True:
 
     opcao = input(menu)
-    if opcao == "d":
+    
+    if opcao == "c":
+        nome = input("Insira seu nome:")
+        data_de_nasc = int(input("Insira sua data de nasc:"))
+        print("Insira seu cpf sem os caracteres. exp: 15478545896")
+        cpf = int(input("Insira seu cpf:"))
+        endereco = input("Insira seu endereço:")
+        #verificar se o usuario ja existe
+        if encontrar_usuario (cpf,usuarios):
+            print("Erro! Já existe um usuário com esse CPF.")
+        else:
+            usuarios.append({"nome": nome, "data_de_nasc": data_de_nasc, "cpf" : cpf, "endereco": endereco})
+            print("Usuário cadastrado com sucesso!")
+            
+    elif opcao == "cc":
+        cpf = int(input("Insira o CPF para criar sua conta corrente:"))
+        usuario = encontrar_usuario(cpf, usuarios)
+        if usuario:
+            conta = {"agencia": agencia, "num_conta": num_conta, "usuario": usuario}
+            contas_correntes.append(conta)
+            print(f"Conta corrente criada com sucesso! Agência: {agencia}, Conta: {num_conta}")
+            num_conta += 1
+        else:
+       
+            print("Erro! Nenhum usuário encontrado com esse CPF.")
+            
+    elif opcao == "d":
 
         valor = float(input("digite um valor:"))
 
@@ -62,6 +100,6 @@ while True:
 
     elif opcao == "f":
         break
-
+    
     else:
         print("Operação inválida, por favor selecione novamente a operação desejada.")
